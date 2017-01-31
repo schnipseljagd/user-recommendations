4159228015ec7dddcde4b9aaeb38feb3bf6cb076
Joscha <joscha@schnipseljagd.org>
2017-01-31 15:09:23 +0100

    fix error handling

--- handler.js
+++ handler.js
@@ -5,28 +5,36 @@ const usersReadAll = require('./libs/users/read-all.js');
 
 module.exports.usersCreate = (event, context, callback) => {
   usersCreate(event, (error, result) => {
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin" : "*"
-      },
-      body: JSON.stringify(result),
-    };
+    if (error) {
+      context.fail(error)
+    } else {
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin" : "*"
+        },
+        body: JSON.stringify(result),
+      };
 
-    context.succeed(response);
+      context.succeed(response);
+    }
   });
 };
 
 module.exports.usersReadAll = (event, context, callback) => {
   usersReadAll(event, (error, result) => {
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin" : "*"
-      },
-      body: JSON.stringify(result),
-    };
+    if (error) {
+      context.fail(error)
+    } else {
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin" : "*"
+        },
+        body: JSON.stringify(result),
+      };
 
-    context.succeed(response);
+      context.succeed(response);
+    }
   });
 };
